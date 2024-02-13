bc4bbcb5a8e9b29d7288349f02adc044da3bb842
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2024-02-12 23:29:31 -0300

    PicPagamento: Start do projeto

---
+++ PicPagamento/src/main/java/com/example/PicPagamento/PicPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PicPagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPagamentoApplication.class, args);
+	}
+
+}
---
+++ PicPagamento/src/test/java/com/example/PicPagamento/PicPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PicPagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
